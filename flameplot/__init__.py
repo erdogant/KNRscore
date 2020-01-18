b757a89a34542485b1d395674c8b54092ffd1424
erdogant <erdogant@gmail.com>
2020-01-18 10:40:06 +0100

    initial commit

---
+++ flameplot/__init__.py
@@ -0,0 +1,21 @@
+import flameplot.flameplot as flameplot
+
+__author__ = 'Erdogan Tasksen'
+__email__ = 'erdogant@gmail.com'
+#__version__ = '0.1.0'
+
+# Automatic version control
+# from ._version import get_versions
+# __version__ = get_versions()['version']
+# del get_versions
+
+
+# module level doc-string
+__doc__ = """
+flameplot - Quantification of local similarity across two maps/embeddings.
+=====================================================================
+
+**flameplot** 
+See README.md file for more information.
+
+"""
