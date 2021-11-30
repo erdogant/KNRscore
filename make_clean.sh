c09eeeec2083dca2789c35eb5e4844457082241d
erdogant <erdogant@gmail.com>
2021-11-30 21:35:51 +0100

    cleaning

---
+++ make_clean.sh
@@ -0,0 +1,24 @@
+echo "Cleaning previous builds first.."
+rm -rf dist
+rm -rf build
+rm -rf flameplot.egg-info
+rm -rf __pycache__
+rm -rf .pytest_cache
+rm -rf .pylint.d
+rm -rf flameplot/data/*.zip
+rm -rf flameplot/data/*.bif
+rm -rf flameplot/data/*.png
+rm -rf flameplot/data/*.txt
+rm -rf flameplot/data/*.json
+rm -rf flameplot/data/*.csv
+rm -rf flameplot/__pycache__
+rm -rf flameplot/helpers/__pycache__
+rm -rf flameplot/tests/__pycache__
+rm -rf flameplot/flameplot/*.pkl
+rm -rf flameplot/flameplot/*.html
+rm -rf *.js
+rm -rf *.html
+rm -rf *.css
+rm -rf *.dot
+rm -rf *.png
+rm -rf *.pkl
