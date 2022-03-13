5baad263bb71b26433aaf265c1e64a26f2fdd12f
erdogant <erdogant@gmail.com>
2022-03-13 22:10:48 +0100

    update examples

--- flameplot/examples.py
+++ flameplot/examples.py
@@ -3,6 +3,30 @@ from sklearn import (manifold, decomposition)
 import numpy as np
 
 # %%
+# Load libraries
+from sklearn import (manifold, decomposition)
+import pandas as pd
+import numpy as np
+
+# Import library
+import flameplot as flameplot
+
+# Load mnist example data
+X,y = flameplot.import_example()
+
+# PCA: 50 PCs
+X_pca_50 = decomposition.TruncatedSVD(n_components=50).fit_transform(X)
+
+# tSNE: 2D
+X_tsne = manifold.TSNE(n_components=2, init='pca').fit_transform(X)
+
+# Compare PCA(50) vs. tSNE
+scores = flameplot.compare(X_pca_50, X_tsne, n_steps=5)
+
+# Plot
+fig = flameplot.plot(scores, xlabel='PCA (50d)', ylabel='tSNE (2d)')
+
+# %%
 # Load data
 X, y = flameplot.import_example()
 
