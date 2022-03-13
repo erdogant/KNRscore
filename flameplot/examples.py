5bcdd00846e887b17ffbad639ed63918a5ef49df
erdogant <erdogant@gmail.com>
2022-03-13 20:18:26 +0100

    update exmaples

--- flameplot/examples.py
+++ flameplot/examples.py
@@ -2,34 +2,32 @@ import flameplot as flameplot
 from sklearn import (manifold, decomposition)
 import numpy as np
 
-# fig    = flameplot.plot(scores)
-# X,y    = flameplot.import_example()
-# fig    = flameplot.scatterd(X[:,0],X[:,1],label=y)
-
-
+# %%
 # Load data
-X,y=flameplot.import_example()
+X, y = flameplot.import_example()
 
-# Make PCA
-X_pca = decomposition.TruncatedSVD(n_components=50).fit_transform(X)
-
-# Make tSNE
-X_tsne = manifold.TSNE(n_components=2, init='pca').fit_transform(X)
-
-# Make random
-X_rand=np.append([np.random.permutation(X_tsne[:,0])],  [np.random.permutation(X_tsne[:,1])], axis=0).reshape(-1,2)
+# Compute embeddings
+embed_pca = decomposition.TruncatedSVD(n_components=50).fit_transform(X)
+embed_tsne = manifold.TSNE(n_components=2, init='pca').fit_transform(X)
 
 # Compare PCA vs. tSNE
-scores=flameplot.compare(X_pca, X_tsne, n_steps=25)
+scores = flameplot.compare(embed_pca, embed_tsne, n_steps=25)
 # plot PCA vs. tSNE
-fig=flameplot.plot(scores, xlabel='PCA', ylabel='tSNE', reverse_cmap=True)
+fig = flameplot.plot(scores, xlabel='PCA', ylabel='tSNE')
+
+
+# %%
+# Make random data
+X_rand=np.append([np.random.permutation(embed_tsne[:,0])],  [np.random.permutation(embed_tsne[:,1])], axis=0).reshape(-1,2)
 
 # Compare random vs. tSNE
-scores=flameplot.compare(X_rand, X_tsne, n_steps=25)
-# plot Random vs. tSNE
-fig=flameplot.plot(scores, xlabel='Random', ylabel='tSNE')
+scores = flameplot.compare(X_rand, embed_tsne, n_steps=25)
+fig = flameplot.plot(scores, xlabel='Random', ylabel='tSNE')
+
+scores = flameplot.compare(X_rand, embed_pca, n_steps=25)
+fig = flameplot.plot(scores, xlabel='Random', ylabel='PCA')
 
 # Scatter
-flameplot.scatter(X_pca[:,0], X_pca[:,1] ,y, title='PCA')
-flameplot.scatter(X_tsne[:,0], X_tsne[:,1], y, title='tSNE')
-flameplot.scatter(X_rand[:,0], X_rand[:,1], y, title='Random')
+flameplot.scatter(embed_pca[:,0], embed_pca[:,1] , label=y, title='PCA')
+flameplot.scatter(embed_tsne[:,0], embed_tsne[:,1], label=y, title='tSNE')
+flameplot.scatter(X_rand[:,0], X_rand[:,1], label=y, title='Random')
